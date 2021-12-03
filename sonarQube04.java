5642ec90c0afabdc1e399c826c5765ed35c2bb56
Nat2194 <94376896+Nat2194@users.noreply.github.com>
2021-12-03 15:50:52 +0100

    Update sonarQube04.java

--- sonarQube04.java
+++ sonarQube04.java
@@ -1,6 +1,6 @@
 public class sonarQube04 {
     private int x;
     private int y = 0;
-    int getX() { return y;}
-    void setX(int val){ y = val; }
+    x = getX() { return y;}
+    x.setX(int val){ y = val; }
 }
